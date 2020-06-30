cf4f9aea0614b45af978053e2b52366b977ecf95
AlthalusAvan <macdara@raid.gg>
2020-06-30 21:46:12 +0100

    "Check" only in debug channel, exclude from help

--- commands/check.js
+++ commands/check.js
@@ -1,11 +1,14 @@
 const checkSubs = require('../helpers/checkSubs');
+const { channels } = require('../config.json');
 
 module.exports = {
   name: 'check',
   description: 'Run a manual check of the whitelist!',
   cooldown: 0,
   guildOnly: true,
+  excludeFromHelp: true,
   execute(message, args) {
+    if (message.channel.id !== channels.debug) return;
     checkSubs();
   },
 };
--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,13 @@ module.exports = {
 
     if (!args.length) {
       data.push("Here's a list of all my commands:");
-      data.push(commands.map((command) => command.name).join(', '));
+      let commandsList = [];
+      commands.map((command) => {
+        if (!command.excludeFromHelp) {
+          commandsList.push(command.name);
+        }
+      });
+      data.push(commandsList.join(', '));
       data.push(
         `\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`
       );

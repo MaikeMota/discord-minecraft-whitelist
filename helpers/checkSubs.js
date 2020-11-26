c1156c0b7e7f4ceb491f011dfcfeaed1318ab6bb
MacDara Roche <mroche7@tcd.ie>
2020-11-26 21:40:23 +0000

    Change to promise structure to allow catches

--- helpers/checkSubs.js
+++ helpers/checkSubs.js
@@ -18,85 +18,85 @@ const checkSubs = () => {
       for (let player of players) {
         console.log(player);
 
-        let discordMember = await message.guild.members.fetch(player.discordID);
-        console.log(discordMember);
+        message.guild.members
+          .fetch(player.discordID)
+          .then((discordMember) => {
+            if (discordMember.roles.cache.has(config.subRole)) {
+              console.log(`${player.minecraftUser} is subscribed`);
+              if (!player.subbed) {
+                console.log(
+                  `${player.minecraftUser} had lost subscription, adding it back and ensuring they're on the whitelist`
+                );
+                updatePlayer(player.discordID, {
+                  subbed: true,
+                  cyclesSinceSubLost: false,
+                });
+                //sendRcon(`whitelist add ${player.minecraftUser}`);
+              }
 
-        if (!discordMember) {
-          console.log(
-            `${player.minecraftUser} is no longer in Discord, removing from the whitelist and the system`
-          );
-          //sendRcon(`whitelist remove ${player.minecraftUser}`);
-          //removePlayer('id', player.discordID);
-          continue;
-        }
+              continue;
+            }
 
-        if (discordMember.roles.cache.has(config.subRole)) {
-          console.log(`${player.minecraftUser} is subscribed`);
-          if (!player.subbed) {
+            if (!player.whitelisted) {
+              console.log(
+                `${player.minecraftUser} is not on the whitelist, ignoring them`
+              );
+              continue;
+            }
+
+            if (player.cyclesSinceSubLost || config.gracePeriod === 0) {
+              let cycles = player.cyclesSinceSubLost;
+              cycles += 1;
+
+              if (cycles >= config.gracePeriod) {
+                console.log(
+                  `${player.minecraftUser} has exceeded the grace period, removing from the whitelist`
+                );
+                //sendRcon(`whitelist remove ${player.minecraftUser}`);
+                /*
+								updatePlayer(player.discordID, {
+									subbed: false,
+									cyclesSinceSubLost: false,
+									whitelisted: false,
+								});
+								*/
+
+                users.removed.push(discordMember.displayName);
+              } else {
+                console.log(
+                  `${player.minecraftUser} is within the grace period, updating their information`
+                );
+                /*
+								updatePlayer(player.discordID, {
+									subbed: false,
+									cyclesSinceSubLost: cycles,
+								});
+								*/
+
+                users.gracePeriod.push(discordMember.displayName);
+              }
+            } else {
+              console.log(
+                `${player.minecraftUser} has started the grace period, updating their information`
+              );
+              /*
+							updatePlayer(player.discordID, {
+								subbed: false,
+								cyclesSinceSubLost: 1,
+							});
+							*/
+
+              users.gracePeriod.push(discordMember.displayName);
+            }
+          })
+          .catch((error) => {
             console.log(
-              `${player.minecraftUser} had lost subscription, adding it back and ensuring they're on the whitelist`
-            );
-            updatePlayer(player.discordID, {
-              subbed: true,
-              cyclesSinceSubLost: false,
-            });
-            //sendRcon(`whitelist add ${player.minecraftUser}`);
-          }
-
-          continue;
-        }
-
-        if (!player.whitelisted) {
-          console.log(
-            `${player.minecraftUser} is not on the whitelist, ignoring them`
-          );
-          continue;
-        }
-
-        if (player.cyclesSinceSubLost || config.gracePeriod === 0) {
-          let cycles = player.cyclesSinceSubLost;
-          cycles += 1;
-
-          if (cycles >= config.gracePeriod) {
-            console.log(
-              `${player.minecraftUser} has exceeded the grace period, removing from the whitelist`
+              `${player.minecraftUser} is no longer in Discord, removing from the whitelist and the system`
             );
             //sendRcon(`whitelist remove ${player.minecraftUser}`);
-            /*
-						updatePlayer(player.discordID, {
-              subbed: false,
-              cyclesSinceSubLost: false,
-              whitelisted: false,
-						});
-						*/
-
-            users.removed.push(discordMember.displayName);
-          } else {
-            console.log(
-              `${player.minecraftUser} is within the grace period, updating their information`
-            );
-            /*
-            updatePlayer(player.discordID, {
-              subbed: false,
-              cyclesSinceSubLost: cycles,
-            });
-						*/
-
-            users.gracePeriod.push(discordMember.displayName);
-          }
-        } else {
-          console.log(
-            `${player.minecraftUser} has started the grace period, updating their information`
-          );
-          /*
-          updatePlayer(player.discordID, {
-            subbed: false,
-            cyclesSinceSubLost: 1,
-					});
-					*/
-
-          users.gracePeriod.push(discordMember.displayName);
-        }
+            //removePlayer('id', player.discordID);
+            continue;
+          });
       }
 
       const embedReport = new Discord.MessageEmbed()
